e8b9351fee9994cf4d12cf80c423822d76a52b1b
skaspy <38936232+skaspy@users.noreply.github.com>
2021-11-05 09:35:04 +0100

    Update auf Real-World-Vue3-END

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
     </div>
+
     <router-view />
   </div>
 </template>
@@ -28,5 +29,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4{
+  font-size: 20px;
 }
 </style>
